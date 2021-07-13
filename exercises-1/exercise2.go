45bb6cccc6b79a0cf1823518da32c468f4aa189c
Claudio <claudiojm92@gmail.com>
2021-07-13 19:51:49 -0300

    new execises

---
+++ exercises-1/exercise2.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
+	/*
+		b. The compiler assigned values to the variables. What are these values called?
+
+		ZERO VALUE
+	*/
+}
